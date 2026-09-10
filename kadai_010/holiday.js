c77e11babb3994123eda5c5d0ff205eb667a568f
nicolezyh <tokyo.nicole.zhang@gmail.com>
2026-09-10 13:40:56 +0900

    holiday.jsを変更

--- kadai_010/holiday.js
+++ kadai_010/holiday.js
@@ -6,9 +6,9 @@ for (let i = 0; i < holidays.length; i++) {
 
 }
 
-let j = 0;
+let i = 0;
 // while文の場合
-while (j < holidays.length) {
-  console.log(holidays[j]);
-  j++;
+while (i < holidays.length) {
+  console.log(holidays[i]);
+  i++;
 }
