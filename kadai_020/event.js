c24bdfebe51adbd6e1da7f6c3799c3edddcdf4cb
nicolezyh <tokyo.nicole.zhang@gmail.com>
2026-09-21 15:02:06 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
+const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
-  const text = document.getElementById('text');
   text.textContent = 'ボタンをクリックしました';
 });
